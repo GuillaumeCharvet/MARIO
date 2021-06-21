a55793b6ae003e47979495b51d6bba309ce193f7
Guillaume Charvet <guicharvet@orange.fr>
2021-06-21 02:03:17 +0200

    menu retours

--- menu.js
+++ menu.js
@@ -29,7 +29,23 @@ var UI_quitter_texte;
 var depth_quitter = 5;
 var quitter_scale = false;
 
+var UI_retour1;
+var UI_retour1_texte;
+var depth_retour1 = 9;
+var UI_retour2;
+var UI_retour2_texte;
+var depth_retour2 = 9;
+
 var stop_menu = false;
+var retour_menu1 = false;
+var retour_menu2 = false;
+
+var controles;
+var reglages;
+var curseur;
+
+var curseur_selec = false;
+var curseur_decal = 0;
 
 class menu extends Phaser.Scene{
     
@@ -57,6 +73,14 @@ class menu extends Phaser.Scene{
         this.load.image('UI_reglages_texte', 'assets/images/UI_reglages_texte.png');
         this.load.image('UI_controles_texte', 'assets/images/UI_controles_texte.png');
         this.load.image('UI_quitter_texte', 'assets/images/UI_quitter_texte.png');
+        this.load.image('UI_retour1', 'assets/images/UI_retour.png');
+        this.load.image('UI_retour1_texte', 'assets/images/UI_retour1_texte.png');
+        this.load.image('UI_retour2', 'assets/images/UI_retour.png');
+        this.load.image('UI_retour2_texte', 'assets/images/UI_retour2_texte.png');
+
+        this.load.image('controles', 'assets/images/controles.png');
+        this.load.image('reglages', 'assets/images/reglages.png');
+        this.load.image('curseur', 'assets/images/curseur.png');
 
     }
     
@@ -75,15 +99,23 @@ class menu extends Phaser.Scene{
         tete.angle += 90;
 
         UI_titre = this.add.sprite(528, 224, 'UI_titre').setDepth(depth_titre);
-        UI_titre_texte = this.add.sprite(528, 224, 'UI_titre_texte').setDepth(depth_titre);
-        UI_jouer = this.add.sprite(458, 264, 'UI_jouer').setDepth(depth_jouer).setInteractive();
-        UI_jouer_texte = this.add.sprite(458, 264, 'UI_jouer_texte').setDepth(depth_jouer).setInteractive();
-        UI_reglages = this.add.sprite(778, 254, 'UI_reglages').setDepth(depth_reglages).setInteractive();
-        UI_reglages_texte = this.add.sprite(778, 254, 'UI_reglages_texte').setDepth(depth_reglages).setInteractive();
-        UI_controles = this.add.sprite(368, 369, 'UI_controles').setDepth(depth_controles).setInteractive();
-        UI_controles_texte = this.add.sprite(348, 369, 'UI_controles_texte').setDepth(depth_controles).setInteractive();
-        UI_quitter = this.add.sprite(678, 384, 'UI_quitter').setDepth(depth_quitter).setInteractive();
-        UI_quitter_texte = this.add.sprite(678, 384, 'UI_quitter_texte').setDepth(depth_quitter).setInteractive();
+        UI_titre_texte = this.add.sprite(508, 87, 'UI_titre_texte').setDepth(depth_titre);
+        UI_jouer = this.add.sprite(443, 250, 'UI_jouer').setDepth(depth_jouer).setInteractive();
+        UI_jouer_texte = this.add.sprite(473, 260, 'UI_jouer_texte').setDepth(depth_jouer).setInteractive();
+        UI_reglages = this.add.sprite(778, 254, 'UI_reglages').setDepth(depth_reglages).setInteractive().setScale(1.2);
+        UI_reglages_texte = this.add.sprite(770, 254, 'UI_reglages_texte').setDepth(depth_reglages).setInteractive();
+        UI_controles = this.add.sprite(368, 369, 'UI_controles').setDepth(depth_controles).setInteractive().setScale(1.2);
+        UI_controles_texte = this.add.sprite(378, 369, 'UI_controles_texte').setDepth(depth_controles).setInteractive();
+        UI_quitter = this.add.sprite(718, 384, 'UI_quitter').setDepth(depth_quitter).setInteractive();
+        UI_quitter_texte = this.add.sprite(718, 374, 'UI_quitter_texte').setDepth(depth_quitter).setInteractive();
+        UI_retour1 = this.add.sprite(338, 384, 'UI_retour1').setDepth(depth_retour1).setInteractive().setVisible(false);
+        UI_retour1_texte = this.add.sprite(343, 378, 'UI_retour1_texte').setDepth(depth_retour1).setInteractive().setVisible(false);
+        UI_retour2 = this.add.sprite(738, 374, 'UI_retour2').setDepth(depth_retour2).setInteractive().setVisible(false);
+        UI_retour2_texte = this.add.sprite(743, 368, 'UI_retour2_texte').setDepth(depth_retour2).setInteractive().setVisible(false);
+
+        controles = this.add.sprite(448+30, 224, 'controles').setDepth(20).setVisible(false);
+        reglages = this.add.sprite(448, 114, 'reglages').setDepth(20).setVisible(false);
+        curseur = this.add.sprite(528, 120, 'curseur').setDepth(20).setInteractive().setVisible(false);
 
         // JOUER
         UI_jouer.on('pointerover', function (pointer) {
@@ -177,8 +209,46 @@ class menu extends Phaser.Scene{
         UI_quitter_texte.on('pointerdown', function (pointer) {
             if(!stop_menu){quitter_scale = true;stop_menu=true;}
         });
+        // RETOUR
+        /*UI_retour1.on('pointerover', function (pointer) {
+            if(!stop_menu){UI_retour1.setDepth(9);
+            UI_retour1_texte.setDepth(9.1);}
+        });
 
-        //setTimeout(function(){game.scene.stop("menu");game.scene.start("main")},1000);
+        UI_retour1_texte.on('pointerover', function (pointer) {
+            if(!stop_menu){UI_retour1.setDepth(9);
+            UI_retour1_texte.setDepth(9.1);}
+        });
+
+        UI_retour1.on('pointerout', function (pointer) {
+            if(!stop_menu){UI_retour1.setDepth(depth_quitter);
+            UI_retour1_texte.setDepth(depth_quitter);}
+        });*/
+
+        UI_retour1.on('pointerdown', function (pointer) {
+            if(true){UI_retour1.setVisible(false);UI_retour1_texte.setVisible(false);reglages.setVisible(false);curseur.setVisible(false);retour_menu1 = true;stop_menu=false;}
+        });
+
+        UI_retour1_texte.on('pointerdown', function (pointer) {
+            if(true){UI_retour1.setVisible(false);UI_retour1_texte.setVisible(false);reglages.setVisible(false);curseur.setVisible(false);retour_menu1 = true;stop_menu=false;}
+        });
+
+        UI_retour2.on('pointerdown', function (pointer) {
+            if(true){UI_retour2.setVisible(false);UI_retour2_texte.setVisible(false);controles.setVisible(false);retour_menu2 = true;stop_menu=false;}
+        });
+
+        UI_retour2_texte.on('pointerdown', function (pointer) {
+            if(true){UI_retour2.setVisible(false);UI_retour2_texte.setVisible(false);controles.setVisible(false);retour_menu2 = true;stop_menu=false;}
+        });
+
+        /*curseur.on('pointerdown', function (pointer) {
+            curseur_selec=true;
+        });
+
+        curseur.on('pointerup', function (pointer) {
+            curseur_selec=false;
+        });*/
+
     }
     
     update ()
@@ -186,13 +256,22 @@ class menu extends Phaser.Scene{
         if (jouer_scale){UI_jouer.scale *= 1.04;};
         if (UI_jouer.scale>20){game.scene.start("main");game.scene.stop("menu")}
 
-        if (reglages_scale && UI_reglages.scale<10){UI_reglages.scale *= 1.04;};
-        if (UI_reglages.scale>10){/*game.scene.stop("menu");game.scene.start("reglages")*/}
-
-        if (controles_scale && UI_controles.scale<=13){UI_controles.scale *= 1.04;};
-        if (UI_controles.scale>13){/*game.scene.start("controles");game.scene.stop("menu")*/}
-
         if (quitter_scale){UI_quitter.scale *= 1.02;};
         if (UI_quitter.scale>40){game.scene.stop("menu")}
+
+        if (reglages_scale && UI_reglages.scale<10 && !retour_menu1 && !retour_menu2){UI_reglages.scale *= 1.04;}
+        if (UI_reglages.scale>10 && !retour_menu1 && !retour_menu2){UI_retour1.setVisible(true);UI_retour1_texte.setVisible(true);reglages.setVisible(true);curseur.setVisible(true);reglages_scale=false}
+
+        if (controles_scale && UI_controles.scale<=13 && !retour_menu1 && !retour_menu2){UI_controles.scale *= 1.04;};
+        if (UI_controles.scale>13 && !retour_menu1 && !retour_menu2){UI_retour2.setVisible(true);UI_retour2_texte.setVisible(true);controles.setVisible(true);controles_scale=false}
+
+        if (retour_menu1 && UI_reglages.scale>1.2){UI_reglages.scale = Math.max(1.2,UI_reglages.scale/1.04);}
+        if (UI_reglages.scale==1.2 && retour_menu1){retour_menu1 = false;}
+
+        if (retour_menu2 && UI_controles.scale>1.2){UI_controles.scale = Math.max(1.2,UI_controles.scale/1.04);}
+        if (UI_controles.scale==1.2 && retour_menu2){retour_menu2 = false;}
+
+        //if (curseur_selec){curseur_decal = pointer.x}
+        //curseur.x = curseur_decal;
     }
 }
